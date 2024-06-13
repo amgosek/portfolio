f4a2d759fb61efb2e67fd56650e52c417525b31d
Hunter McQuaid <89223779+amgosek@users.noreply.github.com>
2024-06-13 16:55:38 -0400

    added new social icons to header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
+import { FaTiktok, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
@@ -16,9 +15,15 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/hunter-mcquaid/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedinIn size={16} />
+              </a>
+              <a href="https://github.com/amgosek" target="_blank" rel="noopener noreferrer">
+                <FaGithub size={16} />
+              </a>
+              <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+                <FaTiktok size={16} />
+              </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,10 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import { FaTiktok, FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import logo from '../assets/img/logo.svg';
 export const NavBar = () => {
 
   const [activeLink, setActiveLink] = useState('home');
@@ -49,11 +45,17 @@ export const NavBar = () => {
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-              </div>
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/hunter-mcquaid/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedinIn size={16} />
+              </a>
+              <a href="https://github.com/amgosek" target="_blank" rel="noopener noreferrer">
+                <FaGithub size={16} />
+              </a>
+              <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+                <FaTiktok size={16} />
+              </a>
+            </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
               </HashLink>
