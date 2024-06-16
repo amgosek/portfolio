2abe08e39b87c2350edd6709baffaaab7d083350
Hunter McQuaid <89223779+amgosek@users.noreply.github.com>
2024-06-16 00:43:03 -0400

    newsletter prepped for 320px width and up

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -6,7 +6,7 @@ export const Newsletter = ({ status, message, onValidated }) => {
 
   useEffect(() => {
     if (status === 'success') clearFields();
-  }, [status])
+  }, [status]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,32 +15,39 @@ export const Newsletter = ({ status, message, onValidated }) => {
     onValidated({
       EMAIL: email
     })
-  }
+  };
 
   const clearFields = () => {
     setEmail('');
-  }
+  };
 
   return (
-      <Col lg={12}>
-        <div className="newsletter-bx wow slideInUp">
-          <Row>
-            <Col lg={12} md={6} xl={5}>
-              <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
-              {status === 'sending' && <Alert>Sending...</Alert>}
-              {status === 'error' && <Alert variant="danger">{message}</Alert>}
-              {status === 'success' && <Alert variant="success">{message}</Alert>}
-            </Col>
-            <Col md={6} xl={7}>
-              <form onSubmit={handleSubmit}>
-                <div className="new-email-bx">
-                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
-                  <button type="submit">Submit</button>
-                </div>
-              </form>
-            </Col>
-          </Row>
-        </div>
-      </Col>
-  )
+    <Col lg={12}>
+      <div className="newsletter-bx wow slideInUp">
+        <Row>
+          <Col lg={6} md={12} className="mb-4">
+            <h3>Subscribe to my Newsletter<br />for a weekly fun fact!</h3>
+          </Col>
+          <Col lg={6} md={12}>
+            <form onSubmit={handleSubmit}>
+              <div className="new-email-bx">
+                {status === 'sending' && <Alert>Sending...</Alert>}
+                {status === 'error' && <Alert variant="danger">{message}</Alert>}
+                {status === 'success' && <Alert variant="success">{message}</Alert>}
+                <input 
+                  value={email} 
+                  type="email" 
+                  name="email" 
+                  autoComplete="email" 
+                  onChange={(e) => setEmail(e.target.value)} 
+                  placeholder="Email Address" 
+                />
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </div>
+    </Col>
+  );
 }
