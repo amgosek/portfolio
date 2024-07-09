a0f9ad6bdb7ce422d3f38e29c1e160af8e5cb465
Hunter McQuaid <89223779+amgosek@users.noreply.github.com>
2024-07-08 21:35:05 -0400

    Update MailchimpForm.js

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -5,8 +5,6 @@ import { Newsletter } from "./Newsletter";
 export const MailchimpForm = () => {
   const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
-  console.log('Mailchimp URL:', postUrl); // Debugging
-
   return (
     <MailchimpSubscribe
       url={postUrl}
